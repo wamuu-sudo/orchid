5f191fcef663619dd16e0bdfc30b0988323d1386
Babilinx <bastiencarretier@protonmail.com>
2022-04-04 19:21:59 +0200

    Update install.sh

--- testing/install.sh
+++ testing/install.sh
@@ -172,6 +172,13 @@ while read line; do
         fi
 done
 }
+create_passwd() # Spécifier le nom de la variable pour $1 et le nom de l'utilisateur en $2
+{
+	echo "${COLOR_WHITE}Saisisez le mot de passe pour l'utilisateur ${2} : ${COLOR_YELLOW}(le mot de passe n'apparaîtrera pas)${COLOR_RESET}"
+	read -s ${1}
+	echo "${COLOR_WHITE} Resaisisez le mot de passe pour le confirmer :${COLOR_RESET}"
+	read -s ${1}2
+}
 
 ###################################################
 # Script start here
@@ -189,7 +196,21 @@ read -p "Pressez ${COLOR_WHITE}[Entrée]${COLOR_RESET} pour commencer l'installa
 # Choix du système
 Select_Orchid_version_to_install
 echo ""
+# Utilisateurs
+echo "${COLOR_GREEN}*${COLOR_RESET} Création des utilisateurs"
 read -p "${COLOR_WHITE}Quel est le nom de l'utilisateur que vous voulez créer : ${COLOR_RESET}" username
+create_passwd "user_pass" ${username}
+# Vérification des mots de passe
+if [[ ${user_pass} != "${user_pass2}" ]]; then echo "${COLOR_YELLOW}Les moots de passe ne concordent pas, reéssayez.${COLOR_RESET}" && create_passwd "${username}_pass" ${username} fi
+echo ""
+echo "Utilisateur root : "
+create_passwd "root_pass" "root"
+# Vérification des mots de passe
+# User
+if [[ ${user_pass} != "${user_pass2}" ]]; then echo "${COLOR_YELLOW}Les moots de passe ne concordent pas, reéssayez.${COLOR_RESET}" && create_passwd "${username}_pass" ${username} fi
+# root
+if [[ ${root_pass} != "${root_pass2}" ]]; then echo "${COLOR_YELLOW}Les moots de passe ne concordent pas, reéssayez.${COLOR_RESET}" && create_passwd "root_pass" "root" fi
+echo ""
 # Passage du clavier en AZERTY
 echo "${COLOR_GREEN}*${COLOR_RESET} Passage du clavier en (fr)."
 loadkeys fr
@@ -319,11 +340,11 @@ chmod +x /mnt/orchid/UEFI-install.sh && chmod +x  /mnt/orchid/BIOS-install.sh &&
 # UEFI
 if [ "$ifbios" = "n" ]
 then
-	chroot /mnt/orchid ./UEFI-install.sh ${ext4_name} ${swap_name} ${EFI_name} ${username}
+	chroot /mnt/orchid ./UEFI-install.sh ${ext4_name} ${swap_name} ${EFI_name} ${username} ${user_pass} ${root_pass}
 # BIOS
 elif [ "$ifbios" = "o" ]
 then
-	chroot /mnt/orchid ./BIOS-install.sh ${ext4_name} ${swap_name} ${disk_name} ${username}
+	chroot /mnt/orchid ./BIOS-install.sh ${ext4_name} ${swap_name} ${disk_name} ${username} ${user_pass} ${root_pass}
 fi
 # Configuration pour DWM
 # no_archive use computer convention: start at 0
