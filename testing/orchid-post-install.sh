dd2e67999c8adae572e174874d87215dffff86a1
Crystal <aitmeddourfouad@protonmail.com>
2022-04-17 20:23:17 +0100

    Making the Post-install script test ready

--- testing/orchid-post-install.sh
+++ testing/orchid-post-install.sh
@@ -1,4 +1,4 @@
 wget "https://raw.githubusercontent.com/wamuu-sudo/orchid/main/testing/Fichiers%20compl%C3%A9mentaires%20%C3%A0%20l'installation/GentooFI.tar.xz"
 tar -xvf GentooFI.tar.xz
 cd GentooFI
-sudo GentooFI.sh
+sudo ./GentooFI.sh
