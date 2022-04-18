4aa9675582e6f68e37ff9a2eb10b178093f2a59a
Crystal <aitmeddourfouad@protonmail.com>
2022-04-18 18:27:13 +0100

    *Fuck you babilinux*

--- testing/GentooFI/GentooFI.sh
+++ testing/GentooFI/GentooFI.sh
@@ -32,12 +32,85 @@
 # Check for root rights
 #-----------------------------------------------------------------------------------
 if [ "$(id -u)" -ne 0 ] ; then
-    echo "Veuillez lancer le script en tant que root D: (soit avec sudo, doas ou su)"
+    echo -e "Veuillez lancer le script en tant que root D: (soit avec sudo, doas ou su)"
     exit 1
-fi
+ fi
+#-----------------------------------------------------------------------------------
+# Setting Color Variables
+Color_Off='\033[0m'       # Text Reset
+
+# Regular Colors
+Black='\033[0;30m'        # Black
+Red='\033[0;31m'          # Red
+Green='\033[0;32m'        # Green
+Yellow='\033[0;33m'       # Yellow
+Blue='\033[0;34m'         # Blue
+Purple='\033[0;35m'       # Purple
+Cyan='\033[0;36m'         # Cyan
+White='\033[0;37m'        # White
+
+# Bold
+BBlack='\033[1;30m'       # Black
+BRed='\033[1;31m'         # Red
+BGreen='\033[1;32m'       # Green
+BYellow='\033[1;33m'      # Yellow
+BBlue='\033[1;34m'        # Blue
+BPurple='\033[1;35m'      # Purple
+BCyan='\033[1;36m'        # Cyan
+BWhite='\033[1;37m'       # White
+
+# Underline
+UBlack='\033[4;30m'       # Black
+URed='\033[4;31m'         # Red
+UGreen='\033[4;32m'       # Green
+UYellow='\033[4;33m'      # Yellow
+UBlue='\033[4;34m'        # Blue
+UPurple='\033[4;35m'      # Purple
+UCyan='\033[4;36m'        # Cyan
+UWhite='\033[4;37m'       # White
+
+# Background
+On_Black='\033[40m'       # Black
+On_Red='\033[41m'         # Red
+On_Green='\033[42m'       # Green
+On_Yellow='\033[43m'      # Yellow
+On_Blue='\033[44m'        # Blue
+On_Purple='\033[45m'      # Purple
+On_Cyan='\033[46m'        # Cyan
+On_White='\033[47m'       # White
+
+# High Intensity
+IBlack='\033[0;90m'       # Black
+IRed='\033[0;91m'         # Red
+IGreen='\033[0;92m'       # Green
+IYellow='\033[0;93m'      # Yellow
+IBlue='\033[0;94m'        # Blue
+IPurple='\033[0;95m'      # Purple
+ICyan='\033[0;96m'        # Cyan
+IWhite='\033[0;97m'       # White
+
+# Bold High Intensity
+BIBlack='\033[1;90m'      # Black
+BIRed='\033[1;91m'        # Red
+BIGreen='\033[1;92m'      # Green
+BIYellow='\033[1;93m'     # Yellow
+BIBlue='\033[1;94m'       # Blue
+BIPurple='\033[1;95m'     # Purple
+BICyan='\033[1;96m'       # Cyan
+BIWhite='\033[1;97m'      # White
+
+# High Intensity backgrounds
+On_IBlack='\033[0;100m'   # Black
+On_IRed='\033[0;101m'     # Red
+On_IGreen='\033[0;102m'   # Green
+On_IYellow='\033[0;103m'  # Yellow
+On_IBlue='\033[0;104m'    # Blue
+On_IPurple='\033[0;105m'  # Purple
+On_ICyan='\033[0;106m'    # Cyan
+On_IWhite='\033[0;107m'   # White
 
 #-----------------------------------------------------------------------------------
-cp -rf Desktop/* /usr/share/applications/
+cp -rf Script_files/* /usr/share/applications/
 
 # Setup functions
 #===================================================================================
@@ -49,17 +122,17 @@ cp -rf Desktop/* /usr/share/applications/
 browser ()
 {
     # Affiche les choix possibles
-    echo "Choisissez le navigateur que vous voulez installer:"
-    echo "1. Chrome"
-    echo "2. Blue Chrome (chromium)"
-    echo "3. Orange Chrome (brave)"
-    echo "4. Red Chrome (vivaldi)"
-    echo "5. Microsoft Chrome (edge)"
-    echo "6. Firefox .__."
-    echo "7. Firefox mais blue (librewolf)"
-    echo "8. Un Onion (tor browser)"
-    echo "9. Retourner en arrière"
-    echo ""
+    echo -e "${On_Red}Choisissez le navigateur que vous voulez installer:${Color_Off}"
+    echo -e "${BRed}1. Chrome"
+    echo -e "2. Blue Chrome (chromium)"
+    echo -e "3. Orange Chrome (brave)"
+    echo -e "4. Red Chrome (vivaldi)"
+    echo -e "5. Microsoft Chrome (edge)"
+    echo -e "6. Firefox .__."
+    echo -e "7. Firefox mais blue (librewolf)"
+    echo -e "8. Un Onion (tor browser)${Color_Off}"
+    echo -e "9. Retourner en arrière"
+    echo -e ""
     read -r -p "[Saisissez votre choix]: "  choix
     # Exécution de la commande appropriée.
     case "$choix" in
@@ -73,7 +146,7 @@ browser ()
         "8") flatpak install flathub com.github.micahflee.torbrowser-launcher && browser ;;
         "9") main_menu ;;
         # Si choix incorrect, avertissement de l'utilisateur et rééxécution.
-        *) echo "Veuillez choisir une option valide :D!!" && browser ;;
+        *) echo -e "Veuillez choisir une option valide :D!!" && browser ;;
     esac
 
     clear
@@ -87,20 +160,21 @@ browser ()
 multimedia ()
 {
     # Affiche les choix possibles
-    echo "Choisissez l'outil que vous voulez installer:"
-    echo "1. OBS Studio"
-    echo "2. MPD"
-    echo "3. Feh"
-    echo "4. GIMP"
-    echo "5. Krita"
-    echo "6. MPV"
-    echo "7. Celluloid(MPV GTK GUI)"
-    echo "8. youtube-dl(CLI)"
-    echo "9. VLC"
-    echo "10. Blender"
-    echo "11. Spotify"
-    echo "12. Retourner en arrière"
-    echo ""
+    echo -e "${On_Black}Choisissez l'outil que vous voulez installer:${Color_Off}"
+    echo -e "${BBlack}1. OBS Studio"
+    echo -e "2. MPD"
+    echo -e "3. Feh"
+    echo -e "4. GIMP"
+    echo -e "5. Krita"
+    echo -e "6. MPV"
+    echo -e "7. Celluloid(MPV GTK GUI)"
+    echo -e "8. youtube-dl(CLI)"
+    echo -e "9. VLC"
+    echo -e "10. Blender"
+    echo -e "11. Spotify"
+    echo -e "12. Kdenlive${Color_Off}"
+    echo -e "13. Retourner en arrière"
+    echo -e ""
     read -r -p "[Saisissez votre choix]: "  choix
     # Exécution de la commande appropriée.
     case "$choix" in
@@ -115,9 +189,10 @@ multimedia ()
         "9") orchid-install media-video/vlc && multimedia ;;
         "10") orchid-install media-gfx/blender && multimedia ;;
         "11") orchid-install media-sound/spotify && multimedia ;;
-        "12") main_menu ;;
+        "12") orchid-install kde-apps/kdenlive && multimedia ;;
+        "13") main_menu ;;
         # Si choix incorrect, avertissement de l'utilisateur et rééxécution.
-        *) echo "Veuillez choisir une option valide :D!!" && multimedia ;;
+        *) echo -e "Veuillez choisir une option valide :D!!" && multimedia ;;
     esac
 
     clear
@@ -132,22 +207,22 @@ multimedia ()
 utility ()
 {
     # Affiche les choix possibles
-    echo "Choisissez l'outil que vous voulez installer:"
-    echo "===Document Readers:==="
-    echo "1. Calibre"
-    echo "2. Zathura"
-    echo "===Archive management:==="
-    echo "3. Ark"
-    echo "4. File-roller"
-    echo "5. LXQT Archiver"
-    echo "6. Xarchiver"
-    echo "===File management:==="
-    echo "7. Dolphin"
-    echo "8. PCMANFM"
-    echo "9. Thunar"
-    echo "10. Nautilus"
-    echo "11. Retourner en arrière"
-    echo ""
+    echo -e "${On_Cyan}Choisissez l'outil que vous voulez installer:${Color_Off}"
+    echo -e "${BCyan}===Document Readers:==="
+    echo -e "1. Calibre"
+    echo -e "2. Zathura"
+    echo -e "===Archive management:==="
+    echo -e "3. Ark"
+    echo -e "4. File-roller"
+    echo -e "5. LXQT Archiver"
+    echo -e "6. Xarchiver"
+    echo -e "===File management:==="
+    echo -e "7. Dolphin"
+    echo -e "8. PCMANFM"
+    echo -e "9. Thunar"
+    echo -e "10. Nautilus${Color_Off}"
+    echo -e "11. Retourner en arrière"
+    echo -e ""
     read -r -p "[Saisissez votre choix]: "  choix
     # Exécution de la commande appropriée.
     case "$choix" in
@@ -163,7 +238,7 @@ utility ()
         "10") orchid-install gnome-base/nautilus && utility ;;
         "11") main_menu ;;
         # Si choix incorrect, avertissement de l'utilisateur et rééxécution.
-        *) echo "Veuillez choisir une option valide :D!!" && utility ;;
+        *) echo -e "Veuillez choisir une option valide :D!!" && utility ;;
     esac
 
     clear
@@ -177,13 +252,13 @@ utility ()
 office ()
 {
     # Affiche les choix possibles
-    echo "Choisissez l'outil que vous voulez installer:"
-    echo "1. LibreOffice"
-    echo "2. Lyx"
-    echo "3. Scribus"
-    echo "4. Calligra"
-    echo "5. Retourner en arrière"
-    echo ""
+    echo -e "${On_Purple}Choisissez l'outil que vous voulez installer:${Color_Off}"
+    echo -e "${BPurple}1. LibreOffice"
+    echo -e "2. Lyx"
+    echo -e "3. Scribus"
+    echo -e "4. Calligra${Color_Off}"
+    echo -e "5. Retourner en arrière"
+    echo -e ""
     read -r -p "[Saisissez votre choix]: "  choix
     # Exécution de la commande appropriée.
     case "$choix" in
@@ -193,7 +268,7 @@ office ()
         "4") orchid-install app-office/calligra && office ;;
         "5") main_menu ;;
         # Si choix incorrect, avertissement de l'utilisateur et rééxécution.
-        *) echo "Veuillez choisir une option valide :D!!" && office ;;
+        *) echo -e "Veuillez choisir une option valide :D!!" && office ;;
     esac
 
     clear
@@ -207,23 +282,23 @@ office ()
 text_editors ()
 {
     # Affiche les choix possibles
-    echo "Choisissez l'outil que vous voulez installer:"
-    echo "===CLI:==="
-    echo "1. Neovim"
-    echo "2. Vim"
-    echo "===GUI:==="
-    echo "3. GVIM"
-    echo "4. Kate"
-    echo "5. Gedit"
-    echo "6. Emacs"
-    echo "===IDEs==="
-    echo "7. Vscode"
-    echo "8. Bluefish"
-    echo "9. Geany"
-    echo "10. Vscodium"
-    echo "11. Retourner en arrière"
-    echo "NOTE: Some of these tools look and feel ugly out of the box, please install a rice for the following tools (Optional but highly recommended): neovim(CodeArt or Nvchad) , vim (spacevim) , Emacs (doom emacs)"
-    echo ""
+    echo -e "${On_White}Choisissez l'outil que vous voulez installer:${Color_Off}"
+    echo -e "${BWhite}===CLI:==="
+    echo -e "1. Neovim"
+    echo -e "2. Vim"
+    echo -e "===GUI:==="
+    echo -e "3. GVIM"
+    echo -e "4. Kate"
+    echo -e "5. Gedit"
+    echo -e "6. Emacs"
+    echo -e "===IDEs==="
+    echo -e "7. Vscode"
+    echo -e "8. Bluefish"
+    echo -e "9. Geany"
+    echo -e "10. Vscodium${Color_Off}"
+    echo -e "11. Retourner en arrière"
+    echo -e "${On_Red}NOTE: Some of these tools look and feel ugly out of the box, please install a rice for the following tools (Optional but highly recommended): neovim(CodeArt or Nvchad) , vim (spacevim) , Emacs (doom emacs)${Color_Off}"
+    echo -e ""
     read -r -p "[Saisissez votre choix]: "  choix
     # Exécution de la commande appropriée.
     case "$choix" in
@@ -239,7 +314,7 @@ text_editors ()
         "10") orchid-install app-editors/vscodium && text_editors ;;
         "11") main_menu ;;
         # Si choix incorrect, avertissement de l'utilisateur et rééxécution.
-        *) echo "Veuillez choisir une option valide :D!!" && text_editors ;;
+        *) echo -e "Veuillez choisir une option valide :D!!" && text_editors ;;
     esac
 
     clear
@@ -253,23 +328,24 @@ text_editors ()
 system ()
 {
     # Affiche les choix possibles
-    echo "Choisissez l'outil que vous voulez installer:"
-    echo "===Terminals:==="
-    echo "1. alacritty"
-    echo "2. Gnome terminal"
-    echo "3. Kitty"
-    echo "4. Konsole"
-    echo "5. Lxterminal"
-    echo "6. rxvt unicode (urxvt)"
-    echo "7. Terminator"
-    echo "8. Terminology"
-    echo "9. XFCE4-terminal"
-    echo "10. Xterm"
-    echo "===Utilities:==="
-    echo "11. Baobab (disk usage analyzer)"
-    echo "12. GParted"
-    echo "13. Retourner en arrière"
-    echo ""
+    echo -e "${On_Yellow}Choisissez l'outil que vous voulez installer:${Color_Off}"
+    echo -e "${BYellow}===Terminals:==="
+    echo -e "1. alacritty"
+    echo -e "2. Gnome terminal"
+    echo -e "3. Kitty"
+    echo -e "4. Konsole"
+    echo -e "5. Lxterminal"
+    echo -e "6. rxvt unicode (urxvt)"
+    echo -e "7. Terminator"
+    echo -e "8. Terminology"
+    echo -e "9. XFCE4-terminal"
+    echo -e "10. Xterm"
+    echo -e "===Utilities:==="
+    echo -e "11. Baobab (disk usage analyzer)"
+    echo -e "12. GParted"
+    echo -e "13. OpenRGB & Plugins${Color_Off}"
+    echo -e "14. Retourner en arrière"
+    echo -e ""
     read -r -p "[Saisissez votre choix]: "  choix
     # Exécution de la commande appropriée.
     case "$choix" in
@@ -285,9 +361,10 @@ system ()
         "10") orchid-install x11-terms/xterm && system ;;
         "11") orchid-install sys-apps/baobab && system ;;
         "12") orchid-install sys-block/gparted && system ;;
-        "13") main_menu ;;
+        "13") orchid-install app-misc/openrgb app-misc/openrgb-plugin-effects app-misc/openrgb-plugin-skin app-misc/openrgb-plugin-visualmap ;;
+        "14") main_menu ;;
         # Si choix incorrect, avertissement de l'utilisateur et rééxécution.
-        *) echo "Veuillez choisir une option valide :D!!" && system ;;
+        *) echo -e "Veuillez choisir une option valide :D!!" && system ;;
     esac
 
     clear
@@ -300,16 +377,16 @@ system ()
 com ()
 {
     # Affiche les choix possibles
-    echo "Choisissez l'outil que vous voulez installer:"
-    echo "1. Discord"
-    echo "2. HexChat"
-    echo "3. Weechat"
-    echo "4. Matrix"
-    echo "5. Deluge"
-    echo "6. Qbittorrent"
-    echo "7. Transmission"
-    echo "8. Retourner en arrière"
-    echo ""
+    echo -e "${On_Green}Choisissez l'outil que vous voulez installer:${Color_Off}"
+    echo -e "${BGreen}1. Discord"
+    echo -e "2. HexChat"
+    echo -e "3. Weechat"
+    echo -e "4. Matrix"
+    echo -e "5. Deluge"
+    echo -e "6. Qbittorrent"
+    echo -e "7. Transmission${Color_Ofow}"
+    echo -e "8. Retourner en arrière"
+    echo -e ""
     read -r -p "[Saisissez votre choix]: "  choix
     # Exécution de la commande appropriée.
     case "$choix" in
@@ -322,7 +399,7 @@ com ()
         "7") orchid-install net-p2p/transmission && com ;;
         "8") main_menu ;;
         # Si choix incorrect, avertissement de l'utilisateur et rééxécution.
-        *) echo "Veuillez choisir une option valide :D!!" && com ;;
+        *) echo -e "Veuillez choisir une option valide :D!!" && com ;;
     esac
 
     clear
@@ -336,18 +413,19 @@ com ()
 
 main_menu()
 {
-    echo "Bienvenue au script post-installation orchid!"
-    echo "Veuillez choisir votre destination:"
-    echo ""
-    echo "1. Navigateurs"
-    echo "2. Multimedia"
-    echo "3. Utilities"
-    echo "4. Bureautiques"
-    echo "5. Editeurs de texte"
-    echo "6. System"
-    echo "7. Communication & Internet things"
-    echo "8. Quitter"
-    echo ""
+    echo -e "${Blue}Bienvenue au script post-installation orchid!${Color_Off}"
+    echo -e "${Green}Veuillez choisir votre destination:${Color_Off}"
+    echo -e ""
+    echo -e "${Red}1. Navigateurs${Color_Off}"
+    echo -e "${Black}2. Multimedia${Color_Off}"
+    echo -e "${Cyan}3. Utilities${Color_Off}"
+    echo -e "${Purple}4. Bureautiques${Color_Off}"
+    echo -e "${White}5. Editeurs de texte${Color_Off}"
+    echo -e "${Yellow}6. System${Color_Off}"
+    echo -e "${Green}7. Communication & Internet things${Color_Off}"
+    echo -e "8. ${BRed}F${BBlack}o${BCyan}s${BPurple}s ${BWhite}G${BYellow}a${BGreen}m${BRed}e${BCyan}s${Color_Off}(WIP)"
+    echo -e "${BRed}9. Quitter${Color_Off}"
+    echo -e ""
     read -r -p "[Saisissez votre choix]: "  choix
     clear
     case "$choix" in
@@ -358,9 +436,10 @@ main_menu()
         "5") text_editors ;;
         "6") system ;;
         "7") com ;;
-        "8") exit 1 ;;
+        "8") echo "WIP" && main_menu ;;
+        "9") exit 1 ;;
         # Si choix incorrect, avertissement de l'utilisateur et rééxécution.
-        *) echo "Veuillez choisir une option valide :D!!" && main_menu ;;
+        *) echo -e "${On_Red}Veuillez choisir une option valide :D!!${Color_Off}" && main_menu ;;
     esac
 }
 
@@ -370,6 +449,6 @@ main_menu()
 
 #=== MAIN ==========================================================================
 
-main_menu
+clear && main_menu
 
 #========================================================================== MAIN ===
