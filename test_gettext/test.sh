8fdb472bf567ddfa80cb54001dce94d54e56eecf
Ferry Jérémie <ferryjeremie@free.fr>
2022-06-28 22:42:07 +0200

    a quick example

--- test_gettext/test.sh
+++ test_gettext/test.sh
@@ -1,14 +1,8 @@
-#!/usr/bin/env bash
-
-#alias GETTEXT='gettext "test"'
-
-. gettext.sh
-
-export TEXTDOMAIN=test
-export TEXTDOMAINDIR=$PWD/locale
-
-pwd=$PWD
-
-gettext "Hello World!"
-eval_gettext "Current dir: \$pwd"
-gettext "End of the script"
+#!/bin/bash
+export LC_ALL='fr_FR.UTF-8'
+export LANG='fr_FR.UTF-8'
+export LANGUAGE='fr_FR.UTF-8'
+export TEXTDOMAIN='orchid'
+export TEXTDOMAINDIR='/home/jferry/projects/orchid/test_gettext/locale'
+LANG_HELLO_WORLD="$( /usr/bin/gettext "orchid" "End of the script" )"
+echo $LANG_HELLO_WORLD
