0293adff376bf039ad1e24e13aaa6da975ef8a1e
Bastien <77548264+Babilinx@users.noreply.github.com>
2022-07-02 18:44:00 +0200

    Update links

--- install/post-install/orchid-post-install.sh
+++ install/post-install/orchid-post-install.sh
@@ -1,4 +1,3 @@
-wget "https://raw.githubusercontent.com/wamuu-sudo/orchid/main/testing/Fichiers%20compl%C3%A9mentaires%20%C3%A0%20l'installation/GentooFI.tar.xz"
-tar -xvf GentooFI.tar.xz
+wget "https://raw.githubusercontent.com/wamuu-sudo/orchid/main/install/post-install/files/GentooFI.sh" -O GentooFI/GentooFI.sh
 cd GentooFI
 sudo ./GentooFI.sh
